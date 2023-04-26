6ed9ab7c2a7f9830dd4ba0e6b72bec951cf6a294
kanishkamuthu <83972268+kanishkamuthu@users.noreply.github.com>
2023-04-26 23:16:45 +0530

    stage 2

--- advanced-java/src/application/testClass.java
+++ advanced-java/src/application/testClass.java
@@ -4,7 +4,7 @@ public class testClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" hello World ....");
+		System.out.println(" hello World .... 2");
 	}
 
 }
